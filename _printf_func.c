7715a417016273e4600eb9b0f6b00179ffa61794
Arrue Marianne <5608@holbertonstudents.com>
2022-11-08 06:44:19 -0800

    first try

--- _printf_func.c
+++ _printf_func.c
@@ -1,1 +1,47 @@
 #
+#include "main.h"
+
+/**
+ * pr_c- function that print %s
+ * @match: type
+ */
+
+void pr_c(va_list match)
+{
+	char ctoprint;
+
+	ctoprint = va_arg(match, int);
+	_putchar(ctoprint);
+}
+
+/**
+ * pr_s- function that print %s
+ * @match: type de char to print
+ */
+
+void pr_s(va_list match)
+{
+	int k = 0;
+	char *toprint;
+
+	toprint = va_arg(match, char *);
+
+	if (toprint == NULL)
+		return;
+	else
+		for (k = 0; toprint[k] != '\0'; k++)
+		{
+			_putchar(toprint[k]);
+		}
+}
+
+/**
+ * pr_p- function that print %
+ * @match: pourcent
+ */
+
+void pr_p(va_list match)
+{
+	(void) match;
+	_putchar('%');
+}
--- main.h
+++ main.h
@@ -1,1 +1,40 @@
-#
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * struct typeprint- Struct tp
+ * @vp: variable to print
+ * @f: associate function
+ */
+typedef struct typeprint
+{
+        char *vp;
+        void (*f)(va_list);
+} typ;
+
+/* f° qui appelle la f°print correspondante au char */
+int _printf(const char *format, ...);
+
+/* f° qui print %s */
+void pr_s(va_list to_match);
+
+/* f° qui print %c */
+void pr_c(va_list to_match);
+
+/* f° qui print %d ou %i */
+void pr_di(va_list to_match);
+
+/* f° qui print % si presence de %, %% ou \% */
+void pr_p(va_list to_match);
+
+/* f° putchar like */
+int _putchar(char c);
+
+#endif
