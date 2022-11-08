3c86ad14baaf03fc40521989f0201467229a2f19
Marine <5641@holbertonstudents.com>
2022-11-08 16:05:49 +0100

    fixing conflict

--- _printf.c
+++ _printf.c
@@ -1,1 +1,52 @@
-#
+#include "main.h"
+
+/**
+ * _printf- function that call function corresponding to %char
+ * @format: pointer type of arg to print
+ *
+ * Return: charactere
+ */
+
+int _printf(const char *format, ...)
+{
+	int nb = 0; /* nb : number of % read in format*/
+	int lenformat, i, j;
+
+	typ match[] = {
+		{"c", pr_c},
+		{"s", pr_s},
+		{"%", pr_p},
+		{NULL, NULL},
+	};
+
+	va_list to_match;
+
+	lenformat = strlen(format);
+	i = 0;
+	j = 0;
+	va_start(to_match, format);
+
+	while (i < lenformat)
+	{
+		if (format[i] == '%')
+		{
+			nb = nb + 1;
+			while (j < 4)
+			{
+				if (format[i + 1] == *match[j].vp)
+				{
+					match[j].f(to_match);
+					break;
+				}
+				j++;
+			}
+			i = i + 1;
+		}
+		else
+			_putchar(format[i]);
+		i++;
+	}
+	va_end(to_match);
+	/*_putchar('\n');*/
+	return (0);
+}
--- main.h
+++ main.h
@@ -6,23 +6,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
- *struct typeprint- Struct tp
- *@vp: variable to print
- *@f: associate function
+ * struct typeprint- Struct tp
+ * @vp: variable to print
+ * @f: associate function
  */
 typedef struct typeprint
 {
-	char *vp;
-	void (*f)(va_list);
-} tp;
+        char *vp;
+        void (*f)(va_list);
+} typ;
 
 /* f° qui appelle la f°print correspondante au char */
 int _printf(const char *format, ...);
 
-/* f° qui print %c ou %s */
-void pr_cs(va_list to_match);
+/* f° qui print %s */
+void pr_s(va_list to_match);
+
+/* f° qui print %c */
+void pr_c(va_list to_match);
 
 /* f° qui print %d ou %i */
 void pr_di(va_list to_match);
